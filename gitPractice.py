99cc5bd16d99f2febbc1587dbec197a1e787782e
ShabbirMinhas <shabbirminhas@gmails>
2025-12-19 17:54:18 +0500

    commit message

--- gitPractice.py
+++ gitPractice.py
@@ -1,2 +1,8 @@
-# Tools and Techiniqes
-import numpy as py
+# this is comment Tools and Techiniqes
+import numpy as np
+print('Load Data')
+
+data = [1, 2, 3, 4, 5]
+mean = np.mean(data)
+print(f'The mean is: {mean}')
+print('All done!)"
